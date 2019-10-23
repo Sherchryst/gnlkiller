214b2ed6325857816651c2767918c47e314d7fec
Sherchryst GAH <sgah@e1r10p12.42.fr>
2019-10-23 17:15:37 +0200

    first commit

---
+++ src/destructeur.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   destructeur.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sgah <sgah@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/23 16:08:56 by sgah              #+#    #+#             */
+/*   Updated: 2019/10/23 16:19:09 by sgah             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#include <sys/types.h>
+#include "../get_next_line.h"
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+
+__attribute__((destructor)) void	lol()
+{
+	while (1);
+}
+
+int		main(int ac, char ** av)
+{
+	(void)ac;
+	int		fd = open(av[1], O_RDONLY);
+	char	*l;
+	int		n;
+
+	while ((n = get_next_line(fd, &l)) == 1)
+	{
+		printf("%s\n", l);
+		free(l);
+	}
+	printf("%d\n", n);
+	lol();
+	return (0);
+}
+
+
